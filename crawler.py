21255abc4b1e05631f5384f6b26667758350cf2e
Burla Alexandru <alex.burla@rocketmail.com>
2022-05-19 19:22:26 +0300

    implementing crawler with selenium

--- crawler.py
+++ crawler.py
@@ -1,41 +1,45 @@
-import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import hashlib
 
-URL = "https://sibiucityapp.ro/ro/places?cat_type=expriente-in-sibiu_1541573835"
-page = requests.get(URL)
+url_login = 'https://sibiu.eventya.eu/users/login'
+url_app = 'https://sibiu.eventya.eu/app'
+email = ''
+password = ''
 
-print(page.text)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id="places_list_container")
-print(results)
+options = Options()
+options.headless = True
 
-# job_elements = results.find_all("div", class_="card-content")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-# python_jobs = results.find_all(
-#     "h2", string=lambda text: "python" in text.lower()
-# )
+def login():
+    driver.get(url_login)
+    driver.find_element(By.ID, 'user_email').send_keys(email)
+    driver.find_element(By.ID,'user_password').send_keys(password)
+    driver.find_element(By.ID,'sign_in').click()
 
-# python_job_elements = [
-#     h2_element.parent.parent.parent for h2_element in python_jobs
-# ]
+def logout():
+    driver.get(url_app)
+    driver.find_element(By.ID, 'user_dropdown_toggle').click()
+    driver.find_element(By.XPATH, '/html/body/header/nav/div/div[3]/div/a[3]').click()
+    driver.close()
 
-jobs_hash = hashlib.md5(results.encode('utf-8'))
+def scrape():
+    driver.get('https://sibiu.eventya.eu/app/incidents/incidents?status_key=11')
+    html_page = driver.page_source
+    #print(html_page)
+    soup = BeautifulSoup(html_page, 'html.parser')
+    rapoarte = soup.find_all('div', {'class': 'list-group-item list-group-item-action text-muted' })
+    for raport in rapoarte:
+        media_div = raport.find('div', {'class': 'media'})
+        link=media_div.find('a', {'class': 'stretched-link'})['href']
+        number=media_div.find('div', {'class': 'class="col-lg-2 col-md-4 col-sm-4"'})
+        print()
+        break
 
-print(jobs_hash.hexdigest(), end='\n'*2)
-
-# for python_job_element in python_job_elements:
-#     title_element = python_job_element.find("h2", class_="title")
-#     company_element = python_job_element.find("h3", class_="company")
-#     location_element = python_job_element.find("p", class_="location")
-#     links = python_job_element.find_all("a")
-
-#     for link in links:
-#         if link.text == "Apply":
-#             link_url = link["href"]
-#             print(link_url)
-
-#     print(title_element.text.strip())
-#     print(company_element.text.strip())
-#     print(location_element.text.strip())
-#     print()
+login()
+scrape()
+logout()
